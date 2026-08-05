575c0450d39fe60225aca83c6a60c39bd5e13f89
William Gomes <william.desenv@gmail.com>
2026-08-05 09:45:30 -0400

    Update

--- src/main/java/com/algaworks/algashop/billing/infrastructure/utility/mapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algashop/billing/infrastructure/utility/mapper/ModelMapperConfig.java
@@ -4,7 +4,6 @@ import com.algaworks.algashop.billing.application.utility.Mapper;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.convention.NamingConventions;
-import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
